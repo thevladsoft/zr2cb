71ef570d5a1768cf4482acc4cd709a20b4697c05
vidler <thevladsoft2@gmail.com>
2018-05-16 12:58:13 -0400

    Release

---
+++ install-user.sh
@@ -0,0 +1,4 @@
+cp ziprar2cbzcbr.desktop $HOME/.local/share/kservices5/ServiceMenus/
+cp compresedtocbx $HOME/bin/
+chmod +x $HOME/bin/compresedtocbx
+xdg-icon-resource install --novendor --size 64 speech_balloon.png

42fe3c6405f89ceaa982b6240409258c83e24261
thevladsoft <thevladsoft@gmail.com>
2024-12-29 10:51:14 -0400

    Coreccion para que rar funcione en directorios con caracteres especiales
       Y una modificacon inicial para plasma 6. El deb aun esta para plasma5,
       Cambiar las lineas en ziprar2cbzcbr para que instale ziprar2cbzcbr.desktop
       en el directorio correcto.

---
+++ install-user-plasma6.sh
@@ -0,0 +1,6 @@
+cp ziprar2cbzcbr.desktop $HOME/.local/share/kio/servicemenus/
+cp compresedtocbx $HOME/bin/
+chmod +x $HOME/bin/compresedtocbx
+chmod +x $HOME/.local/share/kio/servicemenus/ziprar2cbzcbr.desktop
+xdg-icon-resource install --novendor --size 64 speech_balloon.png
+kbuildsycoca6
